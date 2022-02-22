4a43aea611964a5840e26e2324c668c433855e1f
LGOTENI <levigoteni@gmail.com>
2022-02-22 16:00:42 +0100

    Refactoring footer and select all with Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Footer from "./components/Footer"
 import Input from "./components/Input"
